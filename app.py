fdbc13d7e817f9fb5be1d09d42a6d071b71bef36
a2c_hyogo <hyogo.atsushi.cs@daiichisankyo.co.jp>
2023-07-31 11:58:57 +0900

    change revert aborted

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ import os
 from ast import literal_eval
 
 import requests
+
+
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
